0eb04a313f65f1768cf8118f13153a4a0088d61d
nhan.phan <nhan.phan@hoanghacgroup.com>
2017-06-28 16:48:07 +0700

    update

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nhan.phan on 6/28/2017.
+ */
+$(document).ready(function () {
+    $('.fa-angle-down').click(function(){
+        $(this).toggleClass("fa-angle-up");
+        $(this).toggleClass("fa-angle-down");
+    });
+})
