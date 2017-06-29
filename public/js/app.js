f8ae53397b94474d4f9f4bf3cbeb10c4bb5585fb
nhan.phan <nhan.phan@hoanghacgroup.com>
2017-06-29 11:34:54 +0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,30 @@ $(document).ready(function () {
         $(this).toggleClass("fa-angle-up");
         $(this).toggleClass("fa-angle-down");
     });
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+    $("#navbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 650) {
+            $(".navbar").addClass("bg-active");
+            $(".navbar-brand img").attr("src","public/images/logo-active.png");
+        } else {
+            $(".navbar").removeClass("bg-active");
+            $(".navbar-brand img").attr("src","public/images/logo.png");
+        }
+    });
 })
